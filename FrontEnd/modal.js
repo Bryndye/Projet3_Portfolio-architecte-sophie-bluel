b3ded0cc2aa8ab864cb65bd20dfd83c1e8baf743
Bryndye <hugocluzel17@hotmail.fr>
2023-01-04 19:21:35 +0100

    fix get elements delete-icon

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -5,55 +5,54 @@ buttonsGetEvent = false;
 
 function generateEventOnDeleteButtons(){
   const deleteButtons = document.querySelectorAll('.delete-icon');
-  // console.log(document.getElementById("myModal"));
-  console.log(deleteButtons);
-  let testArray = [0,1,2,3];
-  let iconTest = document.getElementById('deleteIcon');
-  // console.log(1);
+  console.log(deleteButtons[0]);
 
-  // for(let button in testArray)
-  // {
-  //     console.log(button);
+  for(i = 0; i < deleteButtons.length; i++)
+  {
+      console.log(deleteButtons[0]);
 
-  //     iconTest.addEventListener('click', event => {
-  //         // Récupérer l'identifiant de l'image à supprimer
-  //         const id = event.target.dataset.id;
+      deleteButtons[0].addEventListener('click', event => {
+          // Récupérer l'identifiant de l'image à supprimer
+          const id = event.target.dataset.id;
   
-  //         // Envoyer une requête DELETE pour supprimer l'image
-  //         fetch(`http://localhost:5678/api/works/${id}`, {
-  //             method: 'DELETE'
-  //         })
-  //         .then(response => response.json())
-  //         .then(data => {
-  //             // Récupérer l'élément de grille contenant l'image
-  //             let galleryEdit = event.target.closest('.galleryEdit');
-  //             let gallery = event.target.closest('.gallery');
+          // Envoyer une requête DELETE pour supprimer l'image
+          fetch(`http://localhost:5678/api/works/${id}`, {
+              method: 'DELETE',
+              headers: new Headers({
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+              })
+          })
+          .then(response => response.json())
+          .then(data => {
+              // Récupérer l'élément de grille contenant l'image
+              // let galleryEdit = event.target.closest('.galleryEdit');
+              // let gallery = event.target.closest('.gallery');
   
-  //             // Supprimer l'élément de la grille de la page
-  //             galleryEdit.parentNode.removeChild(galleryEdit);
-  //             gallery.parentNode.removeChild(gallery);
-  //         })
-  //         .catch(error => {
-  //             console.error(error);
-  //         });
-  //     });
-  // }
+              // // Supprimer l'élément de la grille de la page
+              // galleryEdit.parentNode.removeChild(galleryEdit);
+              // gallery.parentNode.removeChild(gallery);
+              console.log("Its working");
+          })
+          .catch(error => {
+              console.error(error);
+          });
+      });
+  }
 }
-// generateGallery(true);
-// generateEventOnDeleteButtons();
+generateGallery(true);
+setTimeout(function() {
+  const deleteButtons = document.querySelectorAll('.delete-icon');
+}, 1000); // attendre 1 seconde avant d'exécuter le code
 
 // Afficher la fenêtre modale lorsque le bouton est cliqué
 btns[2].addEventListener('click', function() {
   modal.style.display = 'block';
   if(buttonsGetEvent)
-  {
-    generateGallery(true);
-    generateEventOnDeleteButtons();
-    // console.log(document.querySelectorAll('.delete-icon'));
+  {;
       return;
   }
   buttonsGetEvent = true;
-
+  generateEventOnDeleteButtons();
 });
 
 // Fermer la fenêtre modale lorsque la croix est cliquée
